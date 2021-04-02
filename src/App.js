42e2bf8d689c7e5254565334a7d5b406816cb1b1
Iana Dragomir <yanadragomir@mail.ru>
2021-04-02 17:12:34 +0300

    Created a SearchBox component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Component } from 'react';
 import { CardList } from "./components/card-list/card-list.component"
+import { SearchBbox } from "./components/search-box/search-box.component"
 
 class App extends Component {
 
@@ -8,7 +9,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     }
   }
 
@@ -19,10 +21,21 @@ class App extends Component {
     .then(() => console.log(this.state.monsters)) // ()
   }
 
+
   render(){
+
+    const { monsters, searchField } = this.state;
+    const filterMonsters = monsters.filter( monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <SearchBbox 
+          placeholder='search monster' 
+          handleChange={event => this.setState({ searchField: event.target.value })}
+        />
+        <CardList monsters={filterMonsters} />
       </div>
     );
   }
